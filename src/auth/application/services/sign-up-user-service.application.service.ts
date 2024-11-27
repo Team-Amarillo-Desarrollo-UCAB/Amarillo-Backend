1e23c044f5c2fc2ec3b39227dd040f0eb9f1c2b0
Nadine Chancay <109122203+NavileenXD@users.noreply.github.com>
2024-11-27 14:52:49 -0400

    Update Auth module

---
+++ src/auth/application/services/sign-up-user-service.application.service.ts
@@ -0,0 +1,57 @@
+// import { IApplicationService } from "src/common/Application/application-services/application-service.interface";
+// import { Result } from "src/common/Domain/result-handler/Result";
+// import { IdGenerator } from "src/common/Application/Id-generator/id-generator.interface";
+// import { IEventHandler } from "src/common/Application/event-handler/event-handler.interface";
+// import { SignUpEntryDto } from "../dto/entry/sign-up-entry.application.dto";
+// import { SignUpResponseDto } from "../dto/response/sign-up-response.application.dto";
+// import { IUserRepository } from "src/user/domain/repositories/user-repository.interface";
+// import { User } from "src/user/domain/user";
+// import { UserId } from "src/user/domain/value-objects/user-id";
+// import { UserName } from "src/user/domain/value-objects/user-name";
+// import { UserPhone } from "src/user/domain/value-objects/user-phone";
+// import { UserEmail } from "src/user/domain/value-objects/user-email";
+
+// export class SignUpUserApplicationService implements IApplicationService<SignUpEntryDto, SignUpResponseDto> {
+    
+//     private readonly userRepository: IUserRepository
+//     private readonly uuidGenerator: IdGenerator<string>
+//     private readonly eventHandler: IEventHandler
+        
+//     constructor(
+//         eventHandler: IEventHandler,
+//         userRepository: IUserRepository,
+//         uuidGenerator: IdGenerator<string>,
+//     ){
+//         this.userRepository = userRepository
+//         this.uuidGenerator = uuidGenerator
+//         this.eventHandler = eventHandler
+//     }
+    
+//     async execute(signUpDto: SignUpEntryDto): Promise<Result<SignUpResponseDto>> {
+        
+//         const findResult = await this.userRepository.verifyUserExistenceByEmail( signUpDto.email )
+//         if ( !findResult.isSuccess() ) return Result.fail( findResult.Error, findResult.StatusCode, findResult.Message )
+
+//         const idUser = await this.uuidGenerator.generateId()
+
+//         const create = User.create(
+//             UserId.create(idUser), 
+//             UserName.create(signUpDto.name), 
+//             UserPhone.create(signUpDto.phone), 
+//             UserEmail.create(signUpDto.email) 
+//         )
+//         const userResult = await this.userRepository.saveUserAggregate( create )
+//         if ( !userResult.isSuccess() ) return Result.fail( userResult.Error, userResult.StatusCode, userResult.Message )        
+        
+//         await this.eventHandler.publish( create.pullEvents() )
+
+//         const answer = { 
+//             id: idUser,
+//             email: signUpDto.email,
+//             name: signUpDto.name 
+//         }
+//         return Result.success(answer, 200)
+//     }
+   
+//     get name(): string { return this.constructor.name }
+// }
--- src/auth/infraestructure/controller/auth.controller.ts
+++ src/auth/infraestructure/controller/auth.controller.ts
@@ -10,6 +10,8 @@ import { IJwtGenerator } from "src/common/application/jwt-generator/jwt-generato
 import { UuidGenerator } from "src/common/infraestructure/id-generator/uuid-generator";
 import { JwtGenerator } from "../jwt/jwt-generator";
 import { EncryptorBcrypt } from "src/common/infraestructure/encryptor/encryptor-bcrypt";
+//import { OdmUserEntity } from "src/user/infraestructure/entities/odm-entities/odm-user.entity";
+//import { InjectModel } from "@nestjs/mongoose";
 
 @ApiTags('auth')
 @Controller('auth')
@@ -19,7 +21,16 @@ export class AuthController {
     private readonly tokenGenerator: IJwtGenerator<string>
     private readonly encryptor: IEncryptor
 
+/*
+    private readonly ormAccountRepository: IAccountRepository<OrmUser>
+    private readonly odmAccountRepository: IAccountRepository<OdmUserEntity>
+    private readonly userRepository: IUserRepository
+    private readonly syncroInfraUser: Querysynchronizer<OrmUser>
+    */
+    private secretCodes = []
+
     constructor(
+       // @InjectModel('User') private userModel: Model<OdmUserEntity>,
         @Inject('DataSource') private readonly dataSource: DataSource,
         private jwtAuthService: JwtService
     ) {
@@ -27,5 +38,139 @@ export class AuthController {
         this.uuidGenerator = new UuidGenerator()
         this.tokenGenerator = new JwtGenerator(jwtAuthService)
         this.encryptor = new EncryptorBcrypt()
+
+        /*
+         this.syncroInfraUser = new AccountQuerySynchronizer( new OdmUserRepository( userModel ), userModel )
+        this.userRepository = new OrmUserRepository( new OrmUserMapper(), dataSource )
+        this.odmAccountRepository = new OdmAccountRepository( userModel )
+        this.ormAccountRepository = new OrmAccountRepository( dataSource )
+        */
     }
+/*
+    @Post('register')
+    @ApiOkResponse({ description: 'Registrar un nuevo usuario en el sistema', type: SignUpUserSwaggerResponseDto })
+    async signUpUser(@Body() signUpDto: SignUpUserEntryInfraDto) {
+        var data = { userId: 'none', ...signUpDto }
+        if ( !data.type ) data = { type: 'CLIENT', ...data }
+
+        const plainToHash = await this.encryptor.hashPassword(signUpDto.password)
+
+        const emailSender = new WelcomeSender()
+        emailSender.setVariables( { firstname: signUpDto.name } )
+        
+        const eventBus = EventBus.getInstance()
+        const suscribe = eventBus.subscribe('UserCreated', async (event: UserCreated) => {
+            const ormUser = OrmUser.create( 
+                event.userId, event.userName, event.userPhone, event.userEmail, null, plainToHash, data.type, 
+            )
+            this.ormAccountRepository.saveUser( ormUser )
+            this.syncroInfraUser.execute( ormUser )
+            emailSender.sendEmail( signUpDto.email, signUpDto.name )
+        })
+
+        const signUpApplicationService = new ExceptionDecorator( 
+            new LoggingDecorator(
+                new PerformanceDecorator(    
+                    new SignUpUserApplicationService(
+                        eventBus,
+                        this.userRepository,
+                        this.uuidGenerator
+                    ), 
+                    new NativeLogger(this.logger)
+                ),
+                new NativeLogger(this.logger)
+            ),
+            new HttpExceptionHandler()
+        )
+        const resultService = (await signUpApplicationService.execute({
+            userId: 'none',
+            email: data.email,
+            name: data.name,
+            phone: data.phone,
+        }));
+        (await suscribe).unsubscribe()
+
+        return { id: resultService.Value.id }
+    }
+    
+    @Post('forget/password')
+    @ApiOkResponse({ description: 'Obtener codigo temporal para confirmar usuario', type: ForgetPasswordSwaggerResponseDto })
+    async getCodeForUpdatePasswordUser(@Body() getCodeUpdateDto: ForgetPasswordEntryInfraDto ) {
+        this.cleanSecretCodes()
+        const data = { userId: 'none', ...getCodeUpdateDto, }
+        const service = new ExceptionDecorator( 
+            new LoggingDecorator(
+                new PerformanceDecorator(
+                    new GetCodeUpdatePasswordUserInfraService(
+                        this.ormAccountRepository,
+                        new UpdatePasswordSender(),
+                        new SecretCodeGenerator(),
+                    ), 
+                    new NativeLogger(this.logger)
+                ),
+                new NativeLogger(this.logger)
+            ),
+            new HttpExceptionHandler()
+        )
+        const result = await service.execute(data)
+        this.secretCodes = this.secretCodes.filter( e => e.email != result.Value.email )
+        this.secretCodes.push( result.Value )
+        console.log( this.secretCodes )
+        return { date: result.Value.date }
+    }
+
+    @Put('change/password')
+    @ApiOkResponse({ description: 'Cambiar la contraseña del usuario', type: ChangePasswordSwaggerResponseDto })
+    async changePasswordUser(@Body() updatePasswordDto: ChangePasswordEntryInfraDto ) {     
+        this.cleanSecretCodes()
+        const result = this.signCode(updatePasswordDto.code, updatePasswordDto.email)  
+        if ( !result ) throw new BadRequestException('invalid secret code')
+        const data = { userId: 'none',  ...updatePasswordDto }
+        const service = new ExceptionDecorator( 
+            new LoggingDecorator(
+                new PerformanceDecorator(
+                    new ChangePasswordUserInfraService(
+                        this.ormAccountRepository,
+                        this.encryptor,
+                        this.odmAccountRepository
+                    ), 
+                    new NativeLogger(this.logger)
+                ),
+                new NativeLogger(this.logger)
+            ),
+            new HttpExceptionHandler()
+        )
+        await service.execute(data)
+    }
+    
+    @Post('code/validate')
+    @ApiOkResponse({  description: 'Validar codigo de cambio de contraseña', type: ValidateCodeForgetPasswordSwaggerResponseDto })
+    async validateCodeForgetPassword( @Body() codeValDto: CodeValidateEntryInfraDto ) {  
+        if ( !this.validateCode( codeValDto.code, codeValDto.email ) ) throw new BadRequestException('invalid secret code')
+    }
+
+    private validateCode( code: string, email: string ) {
+        var nowTime = new Date().getTime()
+        var search = this.secretCodes.filter( e => (e.code == code && e.email == email) )
+        if ( search.length == 0 ) return false
+        if ( (nowTime - search[0].date)/1000 >= 300 ) return false   
+        return true
+    }
+
+    private signCode( code: string, email: string ) {
+        var nowTime = new Date().getTime()
+        var search = this.secretCodes.filter( e => (e.code == code && e.email == email) )
+        if ( search.length == 0 ) return false
+        if ( (nowTime - search[0].date)/1000 >= 300 ) return false   
+        this.secretCodes = this.secretCodes.filter( e => (e.code != code && e.email != email) )
+        return true
+    }
+
+    private async cleanSecretCodes() {
+        var nowTime = new Date().getTime()
+        this.secretCodes = this.secretCodes.filter( e => {
+            var diff = (nowTime - e.date)/1000
+            if ( diff <= 600 ) return e
+        })
+    }*/
 }
