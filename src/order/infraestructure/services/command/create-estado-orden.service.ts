957f4943f82c81c003d3a0fed7be2b454c2c69ec
Nadine Chancay <109122203+NavileenXD@users.noreply.github.com>
2024-12-05 22:48:03 -0400

    Integrating changes

--- src/order/infraestructure/services/command/create-estado-orden.service.ts
+++ src/order/infraestructure/services/command/create-estado-orden.service.ts
@@ -5,8 +5,8 @@ import { Estado_Orden } from "src/order/infraestructure/entites/Estado-orden/est
 import { OrderMapper } from "src/order/infraestructure/mappers/order-mapper"
 import { EstadoRepository } from "src/order/infraestructure/repositories/estado.repository"
 import { EstadoOrdenRepository } from "src/order/infraestructure/repositories/estado_orden.repository"
-import { CreateEstadoOrdenServiceEntry } from "src/order/infraestructure/services/DTO/entry/create-estado-orden-entry.dto"
-import { CreateEstadoOrdenServiceResponseDTO } from "src/order/infraestructure/services/DTO/response/create-estado-orden-service-response.dto" 
+import { CreateEstadoOrdenServiceEntry } from "src/order/infraestructure/services/dto/entry/create-estado-orden-entry.dto"
+import { CreateEstadoOrdenServiceResponseDTO } from "src/order/infraestructure/services/dto/response/create-estado-orden-service-response.dto" 
 
 
 export class CreateEstadoOrdenService implements IApplicationService
